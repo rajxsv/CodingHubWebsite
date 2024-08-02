d0eef2c064c86fe8f9154ba0b3b006f06ff8fca3
Rajpreet Singh <rajpreetsingh@Rajpreets-MacBook-Pro.local>
2024-08-03 00:53:52 +0530

    New Loading message

--- src/componenets/Loader.jsx
+++ src/componenets/Loader.jsx
@@ -4,21 +4,22 @@ import { LineWave } from "react-loader-spinner";
 export default function Loader() {
   return (
     <>
-    <div className="flex items-center overflow-clip h-lvh w-full justify-center" >
-      <h1> Since I am using Free tier for Backend Service , it may take 2-5 minutes for the first call to start the server, Kindly wait </h1>
-      <LineWave
-        visible={true}
-        height="100"
-        width="100"
-        color="#4fa94d"
-        ariaLabel="line-wave-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        firstLineColor=""
-        middleLineColor=""
-        lastLineColor=""
+      <div className="flex flex-col justify-center items-center overflow-clip h-lvh w-full">
+        <h1> Since I am using Free tier for Backend Service , it may take 2-5 minutes for the first call to start the
+          server, Kindly wait </h1>
+        <LineWave
+            visible={true}
+            height="100"
+            width="100"
+            color="#4fa94d"
+            ariaLabel="line-wave-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            firstLineColor=""
+            middleLineColor=""
+            lastLineColor=""
         />
-        </div>
+      </div>
     </>
   );
 }
